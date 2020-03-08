46a4ba44c0428dd4738a200e53c95852f3cccc75
parkersprouse <parker@parkersprouse.me>
2020-03-07 23:39:20 -0600

    Finished bootstrapping the new client

--- client/src/main.js
+++ client/src/main.js
@@ -1,13 +1,18 @@
 import Vue from 'vue';
+import '@babel/polyfill';
+import 'roboto-fontface/css/roboto/roboto-fontface.css';
+import '@fortawesome/fontawesome-free/css/all.css';
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  vuetify,
   render: (h) => h(App),
 }).$mount('#app');
