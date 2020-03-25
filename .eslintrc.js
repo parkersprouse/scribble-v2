88900a8689653b0ad13e28517e4d4a919be410ec
Parker Sprouse <parker@parkersprouse.me>
2020-03-25 00:08:15 -0500

    Moved server files up to the root of the project

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,10 @@ module.exports = {
   extends: [
     'airbnb-base',
   ],
+  ignorePatterns: [
+    'client/',
+    'node_modules/',
+  ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
