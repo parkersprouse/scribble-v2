2b95f2efd7e099ed5a110b25206159db1625300f
Parker Sprouse <parker@parkersprouse.me>
2020-03-26 23:00:16 -0500

    Added markdown package

--- client/src/views/ShowScribble.vue
+++ client/src/views/ShowScribble.vue
@@ -13,15 +13,31 @@
         </div>
       </div>
 
-      <div v-else>
+      <div v-else class='scribble'>
         <h2 v-html='scribble.title || "<em>No Title</em>"'
-            class='border-bottom font-weight-bold mb-5 pb-2'></h2>
+            class='border-bottom font-weight-bold mb-4 pb-2'></h2>
+        <b-card class='scribble__content'>
+          <b-card-text v-html='content'></b-card-text>
+        </b-card>
+        <div class='scribble__actions'>
+          <b-button variant='info'><b-icon icon='pencil'></b-icon></b-button>
+          <b-button variant='danger'><b-icon icon='trash'></b-icon></b-button>
+        </div>
       </div>
     </b-container>
   </div>
 </template>
 
 <script>
+import MarkdownIt from 'markdown-it';
+
+const markdown = new MarkdownIt({
+  breaks: true,
+  html: true,
+  linkify: true,
+  typographer: true,
+});
+
 export default {
   name: 'show_scribble',
   data() {
@@ -39,5 +55,27 @@ export default {
         this.error = err.response.data.message;
       });
   },
+  computed: {
+    content() {
+      if (!this.scribble || !this.scribble.content) return '';
+      if (this.scribble.rich_editor) return this.scribble.content;
+      return markdown.render(this.scribble.content);
+    },
+  },
 };
 </script>
+
+<style lang='scss'>
+.scribble {
+  .scribble__content {
+    .card-text {
+      margin-bottom: 0;
+
+      > *:last-child {
+        margin-bottom: 0;
+        padding-bottom: 0;
+      }
+    }
+  }
+}
+</style>
