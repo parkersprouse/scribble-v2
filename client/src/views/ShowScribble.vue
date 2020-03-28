fa7e6949ed065802032fd12c5c2ca83a7628fccb
Parker Sprouse <parker@parkersprouse.me>
2020-03-28 02:58:15 -0500

    Added action buttons to scribble show page

--- client/src/views/ShowScribble.vue
+++ client/src/views/ShowScribble.vue
@@ -7,7 +7,7 @@
       </b-alert>
 
       <div v-else-if='!scribble' class='d-flex flex-wrap justify-content-center mb-3'>
-        <b-spinner label="Loading..."></b-spinner>
+        <b-spinner label='Loading...'></b-spinner>
         <div class='font-weight-bolder mt-3 text-center w-100'>
           Loading Scribble...
         </div>
@@ -20,8 +20,22 @@
           <b-card-text v-html='content'></b-card-text>
         </b-card>
         <div class='scribble__actions'>
-          <b-button variant='info'><b-icon icon='pencil'></b-icon></b-button>
-          <b-button variant='danger'><b-icon icon='trash'></b-icon></b-button>
+          <b-button v-b-tooltip.hover size='sm' title='Edit' variant='outline-primary'>
+            <svg class='bi b-icon bi-pencil-square' width='1em' height='1em' viewBox='0 0 16 16'
+                 fill='currentColor' xmlns='http://www.w3.org/2000/svg'>
+              <path d='M15.502 1.94a.5.5 0 010 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 01.707
+                       0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 00-.121.196l-.805
+                       2.414a.25.25 0 00.316.316l2.414-.805a.5.5 0 00.196-.12l6.813-6.814z' />
+              <path clip-rule='evenodd' fill-rule='evenodd' d='M1 13.5A1.5 1.5 0 002.5 15h11a1.5 1.5
+                                                               0 001.5-1.5v-6a.5.5 0 00-1 0v6a.5.5 0
+                                                               01-.5.5h-11a.5.5 0 01-.5-.5v-11a.5.5
+                                                               0 01.5-.5H9a.5.5 0 000-1H2.5A1.5 1.5
+                                                               0 001 2.5v11z' />
+            </svg>
+          </b-button>
+          <b-button v-b-tooltip.hover size='sm' title='Delete' variant='outline-danger'>
+            <b-icon icon='trash' scale='1.25'></b-icon>
+          </b-button>
         </div>
       </div>
     </b-container>
@@ -75,6 +89,15 @@ export default {
         margin-bottom: 0;
         padding-bottom: 0;
       }
+    }
+  }
+
+  .scribble__actions {
+    margin-top: 1rem;
+    text-align: center;
+
+    > *:not(:last-child) {
+      margin-right: 0.5rem;
     }
   }
 }
